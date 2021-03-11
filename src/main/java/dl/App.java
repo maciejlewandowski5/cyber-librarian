c3a77f994ced1f0fc7ef621dbba354c67fce77ca
maciejlewandowski5 <64683954+maciejlewandowski5@users.noreply.github.com>
2021-03-11 14:36:53 +0100

    Feature most common year was added

--- src/main/java/dl/App.java
+++ src/main/java/dl/App.java
@@ -1,6 +1,7 @@
 package dl;
 
 import dl.extractor.Extractor;
+import dl.extractor.features.MostCommonYear;
 import dl.extractor.features.NLengthCharCounter;
 import dl.extractor.features.TotalWordsNumber;
 import dl.extractor.features.UnitType;
@@ -11,11 +12,9 @@ import java.io.IOException;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException {
+public class App {
+    public static void main(String[] args) throws IOException {
         ArticlesLoader articlesLoader = new ArticlesLoader();
 
         for (Article a : articlesLoader.getArticles()) {
@@ -23,21 +22,22 @@ public class App
             Extractor extractor = new Extractor();
             TotalWordsNumber totalWordsNumber = new TotalWordsNumber();
             UnitType unitType = new UnitType();
-            NLengthCharCounter shortWords= new NLengthCharCounter(0,4);
-            NLengthCharCounter middleWords= new NLengthCharCounter(5,8);
-            NLengthCharCounter longWords= new NLengthCharCounter(9,Integer.MAX_VALUE);
+            NLengthCharCounter shortWords = new NLengthCharCounter(0, 4);
+            NLengthCharCounter middleWords = new NLengthCharCounter(5, 8);
+            NLengthCharCounter longWords = new NLengthCharCounter(9, Integer.MAX_VALUE);
+            MostCommonYear mostCommonYear = new MostCommonYear();
 
             extractor.addFeature(totalWordsNumber);
             extractor.addFeature(unitType);
             extractor.addFeature(shortWords);
             extractor.addFeature(middleWords);
             extractor.addFeature(longWords);
+            extractor.addFeature(mostCommonYear);
             extractor.extract(a);
-            if(extractor.getFeature(1)!=null){
-            System.out.println(extractor.getFeature(2));
-                System.out.println(extractor.getFeature(3));
-                System.out.println(extractor.getFeature(4));
-            };
+            if (extractor.getFeature(1) != null) {
+                System.out.println(extractor.getFeature(5));
+            }
+            ;
         }
 
     }
