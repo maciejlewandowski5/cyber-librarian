8fee0fd51cc00ed692c86d41cc89a73a1fe8feab
KamilDike <Kamil.Dike.96@gmail.com>
2021-03-16 16:44:23 +0100

    more solid.

--- src/main/java/dl/extractor/features/MostFrequent.java
+++ src/main/java/dl/extractor/features/MostFrequent.java
@@ -2,57 +2,25 @@ package dl.extractor.features;
 
 import dl.extractor.Feature;
 import dl.model.MostFrequentFile;
-import dl.parser.Article;
-import dl.parser.Stemmer;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.Collections;
+import java.util.Map;
 
-public class MostFrequent implements Feature {
-    private Map<String, Integer> map;
+public abstract class MostFrequent implements Feature {
+    protected Map<String, Integer> map;
 
-    public MostFrequent(MostFrequentFile filenameEnum) {
-        String filename = "";
-        switch (filenameEnum) {
-            case MONTHS:
-                filename = "months.txt";
-                break;
-            case BUSINESS_ENTITY:
-                filename = "businessEntity.txt";
-                break;
+    protected String checkFilename(MostFrequentFile mostFrequentFile) {
+        switch (mostFrequentFile) {
+            case SEAS:
+                return "seas.txt";
             case STOCK_EXCHANGE:
-                filename = "stockExchange.txt";
-                break;
+                return "stockExchange.txt";
+            case MONTHS:
+                return "months.txt";
+            case BUSINESS_ENTITY:
+                return "businessEntity.txt";
         }
-
-        map = new HashMap<>();
-        loadFile(filename);
-    }
-
-    private void loadFile(String filename) {
-        try (Stream<String> lines = Files.lines(Paths.get("res", filename), Charset.defaultCharset())) {
-            lines.forEachOrdered(line -> {
-                line = Stemmer.stemWord(line.toLowerCase());
-                map.put(line, 0);
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void extract(Article article) {
-        List<String> text = article.getBody();
-        text.forEach(word -> {
-            if (map.containsKey(word)) {
-                Integer count = map.get(word);
-                map.put(word, count + 1);
-            }
-        });
+        return "";
     }
 
     @Override
@@ -61,10 +29,10 @@ public class MostFrequent implements Feature {
             return "";
         }
         String key = Collections.max(map.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getKey();
-        if (map.get(key) == 0) {
-            return "";
-        } else {
+        if (map.get(key) > 0) {
             return key;
+        } else {
+            return "";
         }
     }
 
